461a23a2ff41fefcb306f58a8d1621583edbc1ae
cyoushika <Tears@mm-dhcp-128-120.naist.jp>
2017-03-15 16:25:48 +0900

    The Definer of UUID
    I chose to use CMD and the command “uuidgen” to get uuid.

---
+++ Definer.swift
@@ -0,0 +1,13 @@
+//
+//  Definer.swift
+//  CB_PeripheralTester
+//
+//  Created by zhangzhihua on 2017/3/9.
+//  Copyright © 2017年 zhangzhihua. All rights reserved.
+//
+
+import Foundation
+import CoreBluetooth
+
+let ServiceUUID = CBUUID(string: "70976225-FD79-4C67-AD05-39AD04427083")
+let CharacteristicUUID = CBUUID(string: "F0050622-003F-4330-9849-580502430274")
